3a200fe5f7e75c3c44af60fc368fda3777222193
Robin Kuo <robin.kuo.chin.ting@gmail.com>
2026-02-08 23:05:25 +0800

    Add Umami analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
@@ -27,6 +28,14 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+
+        {/* Umami Analytics */}
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="d65b7cc5-7173-4d5a-91cb-feb0a55140f2"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
